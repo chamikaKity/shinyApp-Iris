14ca523ea5d67c942359625d40633f2b5b471256
chamikaKity <chamika.subashinie@gmail.com>
2017-11-26 02:51:54 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,25 +5,30 @@ shinyUI(fluidPage(
         titlePanel("Prediction of Species from Petal Length and Petal Width"),
         
         p(strong("Description")),
-        p("This famous (Fisher's or Anderson's) iris data set gives the measurements",
+        p("The famous (Fisher's or Anderson's)", span("iris",style="font-family:monospace"),
+          "data set gives the measurements",
           "in centimeters of the variables sepal length and width and petal length and width,",
           "respectively, for 50 flowers from each of 3 species of iris.",
           "The species are Iris setosa, versicolor, and virginica."),
         
-        p("The application uses variables", strong("Species, Petal.Length"), "and",
-          strong("Petal.Width"), "data from the", strong("iris"), "dataset to",
-          "predict the category using a randomforest model as follows.",
-          strong("randomForest(Species~Petal.Length+Petal.Width, data=iris)")),
+        p("This application uses variables", span("Species, Petal.Length",style="font-family:monospace"),
+          "and",span("Petal.Width",style="font-family:monospace"), 
+          "data from the", span("iris",style="font-family:monospace"), "dataset to",
+          "predict the category (",span("Species",style="font-family:monospace"),
+          ") using a randomforest model as follows.",
+          div("randomForest(Species~Petal.Length+Petal.Width, data=iris)",style="font-family:monospace")),
         
         p(strong("How to use")),
-        p("Select values for", strong("Petal.Length"), "and", strong("Petal.Width"), 
+        p("Select values for", span("Petal.Length",style="font-family:monospace"), "and"
+          , span("Petal.Width",style="font-family:monospace"), 
           "using the sliders on the left,","and click", strong("submit."),
           "You will see the chosen input displayed as a blue",
           "circle on the figure and the predicted category below the figure."),
         
         p(strong("Github link for server.R and ui.R code:"),
-          a("https://github.com/chamikaKity/shinyApp-Iris")),
-        
+          a("https://github.com/chamikaKity/shinyApp-Iris",
+            href="https://github.com/chamikaKity/shinyApp-Iris")),
+       
         
         sidebarLayout(
                 sidebarPanel(
